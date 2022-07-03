32283d1d6b83ff199505abdbeb69262b9c5628fb
reme2.718 <103392516+rem2718@users.noreply.github.com>
2022-07-03 13:56:20 +0300

    Update Example.ino

--- Example.ino
+++ Example.ino
@@ -3,15 +3,8 @@
 
 /*
 -A simple example for receiving coordinates(x,y) and sending their signs(positive, negative)
--Create BTM for each serial you will use
--The format for the messages is ?xy&xy; x for start symbols, y for data (to change the const characters check and modify BTM.h)
--The symbols/data can be in any length
--You can use any number of data
--Create Message for each collection of data youre sending/receiving(this class takes only vectors)
--Start symbols can be shared across messages(they are read-only vectors)
--Data arrays have to be unique for each message
--all data are string you can convert them to any type using(.toInt()/.toFloat()/...)
-
+-The format for the messages is ?xy&xy; x for start symbols, y for data
+-Create Message for each collection of data youre sending/receiving(this class takes only vectors
 */
 
 String sm1[2] = {"x=", "y="}; // start symbols arr for the receiving message
